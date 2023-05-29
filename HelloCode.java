00341c4f91ee086367ee1ef4e07fa4b4afb4b0c7
kajalberad <134680433+kajalberad@users.noreply.github.com>
2023-05-29 22:51:20 +0530

    Uploading java  file

---
+++ HelloCode.java
@@ -0,0 +1,17 @@
+public class HelloCode {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello Java Git");
+		  
+		  
+	
+
+	}
+
+}
+
+https://github.com/kajalberad/github-web-1.git
+
+private -
+https://github.com/kajalberad/github-web-3.git
