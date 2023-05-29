fd285579e886eeedc2535a2dbc4e8598272509c2
kajalberad <kajal-prajwal.berad@capgemini.com>
2023-05-29 23:05:59 +0530

    adding file to my branch

--- HelloCode.java
+++ HelloCode.java
@@ -11,7 +11,3 @@ public class HelloCode {
 
 }
 
-https://github.com/kajalberad/github-web-1.git
-
-private -
-https://github.com/kajalberad/github-web-3.git
---
+++ HiCode.java
@@ -0,0 +1,5 @@
+public class HiCode{
+public static void main(Strings args){
+       System.out.println("Hi Java Git");
+ }
+}
